051a6a7118fdde38a4a581125712fa53e78172b8
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-08-31 03:13:29 +0700

    done

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const axios = require('axios');
+const { log } = require('console');
 const app = express();
 const port = 5000;
 
-// ... Khai báo tuyến đường khác (nếu có) ...
 
-app.get('/', (req, res) => {
+// ... Khai báo tuyến đường khác (nếu có) ...
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', async (req, res) => {
     res.send('Hello World!');
 })
 
-app.get('/salary', async (req, res) => {
+app.post('/salary', async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     try {
-        const response = await axios.get('https://www.fhs.com.tw/ads/api/Furnace/rest/json/hr/s16/VNW0014732vkokv2023-06');
+        let id = req.body.id || 'id-not-found';
+        let year = req.body.year || 'year-not-found';
+        let month = req.body.month || 'month-not-found';
+
+        console.log(id);
+        const response = await axios.get('https://www.fhs.com.tw/ads/api/Furnace/rest/json/hr/s16/' + id + 'vkokv' + year + '-' + month);
         res.json({
             data: response.data
         });
