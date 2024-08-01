e0bc3bb15786861ad7dc78a183302544003127f8
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-01 14:49:21 +0700

    chore: Update config.js and index.html

--- config.js
+++ config.js
@@ -1,3 +1,28 @@
 let CONFIG = {
     "path_api": "https://salary-api-af8k.onrender.com/api/link",
 }
+
+let password = "atuan97";
+
+function promptForData() {
+    pass = localStorage.getItem("password-fhs");
+    time = localStorage.getItem("time-fhs");
+    // 1 phút
+    if (pass === password &&  Date.now() - time < 60000) {
+        localStorage.setItem("time-fhs", Date.now());
+        return;
+    }
+     
+    let input = prompt("Vui lòng nhập dữ liệu:");
+    if (input !== password) {
+        alert("Dữ liệu không hợp lệ");
+        window.location.reload();
+    } else {
+        localStorage.setItem("password-fhs", input);
+        localStorage.setItem("time-fhs", Date.now());
+    }
+}
+
+window.onload = function() {
+    promptForData();
+};
